0d2ca312a081f0b657a7c5d7c8cc52a68aeb7ef9
Harry Kituyi <kituyiharry@gmail.com>
2019-03-28 12:54:03 +0300

    Minor Aesthetics

---
+++ apps/wingu_web/assets/js/vue/routes/home/components/CompanyElement.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card hover @click='$router.push(`/b/${company.id}`)'
+    v-ripple class='' style='border-radius: 8px;'>
+    <v-layout column align-center pa-2>
+      <v-avatar data-aos='zoom-in' data-aos-anchor='#stations' 
+        data-aos-anchor-placement='bottom-bottom' :data-aos-delay='(i+1)*100'>
+        <v-img src='/images/sahihi.png' alt=''>
+          <template v-slot:placeholder>
+            <v-layout
+              fill-height
+              align-center
+              justify-center
+              ma-0>
+              <v-progress-circular indeterminate color="orange"></v-progress-circular>
+            </v-layout>
+          </template>
+        </v-img>
+      </v-avatar>
+      <!--<span class='text-xs-center' data-aos='fade' data-aos-anchor='#stations' data-aos-anchor-placement='center-bottom' :data-aos-delay='i*50'>-->
+        <v-chip flat small label color='primary lighten-1' class='white--text'>
+          <v-avatar tile class="deep-orange">
+            <strong>
+              {{ company.name | first }}
+            </strong> 
+          </v-avatar>
+          {{ company.name }}
+        </v-chip>
+      </v-layout>
+    </v-card>
+  </template>
+  <script charset="utf-8">
+export default {
+  name: "CompanyElement",
+  props: ['company'],
+  data: () => ({
+  }),
+  filters: {
+    first(letter){
+      let fl = letter.split(" ")
+      return(fl[0].slice(0,1))
+    }
+  }
+}
+</script>
+<style scoped type="text/css" media="screen">
+.v-card--hover{
+  border-radius: 8px;
+  background-image: linear-gradient(to bottom, #AA076BFF, #61045F00);
+  background-color: #61045fff;
+}
+.v-card--hover:hover{
+  background-color: #aa076bff;
+  transform: translateY(-7.5px);
+}
+
+</style>
--- apps/wingu_web/assets/js/vue/routes/home/components/HomeCard.vue
+++ apps/wingu_web/assets/js/vue/routes/home/components/HomeCard.vue
@@ -37,34 +37,7 @@
               <div
                 data-aos='fade-up' data-aos-anchor='#stations' data-aos-anchor-placement='bottom-bottom'
                 >
-              <v-card hover @click='$router.push(`/b/${i.id}`)'
-                v-ripple color='primary' class='' style='border-radius: 8px;'>
-                <v-layout column align-center pa-2>
-                  <v-avatar data-aos='zoom-in' data-aos-anchor='#stations' 
-                    data-aos-anchor-placement='bottom-bottom' :data-aos-delay='(i+1)*100'>
-                    <v-img src='/images/sahihi.png' alt=''>
-                      <template v-slot:placeholder>
-                        <v-layout
-                          fill-height
-                          align-center
-                          justify-center
-                          ma-0>
-                          <v-progress-circular indeterminate color="orange"></v-progress-circular>
-                        </v-layout>
-                      </template>
-                    </v-img>
-                  </v-avatar>
-                  <!--<span class='text-xs-center' data-aos='fade' data-aos-anchor='#stations' data-aos-anchor-placement='center-bottom' :data-aos-delay='i*50'>-->
-                    <v-chip flat small label color='primary lighten-1' class='white--text'>
-                      <v-avatar tile class="deep-orange">
-                        <strong>
-                          {{ i.name | first }}
-                        </strong> 
-                      </v-avatar>
-                      {{ i.name }}
-                    </v-chip>
-                  </v-layout>
-                </v-card>
+                <CompanyElement :company='i' />
               </div>
                 <!--</span>-->
             </v-flex>
@@ -135,18 +108,14 @@
   </v-card>
 </template>
 <script charset="utf-8">
+import CompanyElement from './CompanyElement.vue'
 export default{
   name: "HomeCard",
+  components: { CompanyElement },
   props: ['showRegister'],
   data: () => ({
     // showRegister: false,
   }),
-  filters: {
-    first(letter){
-      let fl = letter.split(" ")
-      return(fl[0].slice(0,1))
-    }
-  }
 }
 </script>
 <style scoped type="text/css" media="screen">
