c6e7e3f66a54810a3a40139f2076021236ac6a2c
Harry Kituyi <kituyiharry@gmail.com>
2019-04-13 19:54:19 +0300

    Node wizard

---
+++ apps/wingu_web/assets/js/vue/routes/home/subroutes/company_routes/components/FormNode.vue
@@ -0,0 +1,38 @@
+<template> 
+  <v-menu attach offset-y v-model='menu' 
+    :close-on-click='true' :close-on-content-click='false' :nudge-bottom='2' 
+    transition='slide-y-reverse-transition' style='width:100%;'>
+    <v-card slot='activator' flat light color='secondary lighten-3' style='border-radius: 8px;width:100%;'>
+      <v-card-title>
+        {{ node.label }}
+      </v-card-title>
+    </v-card>
+    <div>
+      <div class='up-arrow'/>
+      <v-card style='border-radius: 8px;' flat height='180' tile >
+      </v-card>
+    </div>
+  </v-menu>
+</template>
+<script charset="utf-8">
+export default {
+  name: "FormNode",
+  props: ['node', 'index', 'secname', 'secindex'],
+  data: () => ({
+    menu: false,
+    expand: false
+  }),
+  methods: {
+  }
+}
+</script>
+<style type="text/css" media="screen">
+.up-arrow {
+  border-color: white transparent;
+  border-style: solid;
+  border-width: 0px 7px 7px 7px;
+  margin-left: 10px;
+  height: 0px;
+  width: 0px;
+}
+</style>
--- apps/wingu_web/assets/js/vue/routes/home/subroutes/company_routes/components/FormWizard.vue
+++ apps/wingu_web/assets/js/vue/routes/home/subroutes/company_routes/components/FormWizard.vue
@@ -4,8 +4,40 @@
       <v-layout row wrap>
         <v-flex xs12>
           <v-card color='accent darken-3' flat tile min-height='382'>
-            <v-card-text class='text-xs-center'>
+            <v-card-text style='font-size: 18px;' class=''>
+              <strong>
               Create a section with inputs
+              </strong>
+            </v-card-text>
+            <v-divider />
+            <v-card-text class='py-2 px-0'>
+              <v-layout row style='align-items: flex-start;overflow-x: auto;'  class='px-1'>
+                <!--<v-flex xs6 
+                  mr-2>-->
+                  <div 
+                    v-for='(section,secindex) in sectionNode.sections' :key='secindex' 
+                    class="py-2 px-1">
+                  <v-card 
+                    @click='log'
+                    class='ma-0' 
+                    style='border-radius: 8px;' min-height='270' min-width='330' flat tile color='accent darken-1'>
+                    <v-card-title>
+                      <strong style='font-size: 18px;'>
+                        {{ section.name }}
+                      </strong>
+                    </v-card-title>
+                    <v-divider />
+                    <v-card-text>
+                      <v-layout row wrap> 
+                        <v-flex pa-1 v-for='(node,index) in section.nodes' :key='index' xs12 sm md>
+                          <FormNode :node='node' :index='index' :secname='section.name' :secindex='secindex'/>
+                        </v-flex>
+                      </v-layout>
+                    </v-card-text>
+                  </v-card>
+                  </div>
+                <!--</v-flex>-->
+              </v-layout>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -21,16 +53,45 @@
   </v-card>
 </template>
 <script charset="utf-8">
+import FormNode from './FormNode.vue';
 export default {
   name: "FormWizard",
+  components: { FormNode },
   data: () => ({
     sectionNode: {
       sections: [
         {name: "Personal Information", description: "...", nodes: [
-          {label: "First Name"}
-        ]}
+          {label: "First Name", action: "firstname", long: false},
+          {label: "Last Name", action: "firstname", long: false},
+        ]},
+        {name: "Files", description: "...", nodes: [
+          {label: "First Name", action: "firstname", long: false},
+          {label: "First Name", action: "firstname", long: false},
+          {label: "First Name", action: "firstname", long: false},
+          {label: "First Name", action: "firstname", long: false},
+          {label: "Last Name", action: "firstname", long: false},
+        ]},
+        {name: "Personal Information", description: "...", nodes: [
+          {label: "First Name", action: "firstname", long: false},
+          {label: "Last Name", action: "firstname", long: false},
+          {label: "First Name", action: "firstname", long: false},
+        ]},
+        {name: "Personal Information", description: "...", nodes: [
+          {label: "First Name", action: "firstname", long: false},
+          {label: "Last Name", action: "firstname", long: false},
+        ]},
       ]
     }
-  })
+  }),
+  methods: {
+    log(e){
+      e.target.focus()
+    },
+    createSimpleID(nodeLabel, index, sectionIndex, sectionName){
+      return sectionIndex.toString().concat(sectionName.concat(index.toString().concat(nodeLabel)))
+    }
+  }
 }
 </script>
+<style type="text/css" media="screen">
+</style>
