d96a72aee95fae60c9dd0aa35be8f14f440bc6cf
Harry Kituyi <kituyiharry@gmail.com>
2019-05-03 13:04:16 +0300

    New tabs

---
+++ apps/wingu_web/assets/js/vue/routes/home/subroutes/company_routes/Records.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid :class='$vuetify.breakpoint.smAndDown ? "mygradient pa-0 mt-5" :  "mygradient mt-5" '>
+    <v-layout row wrap fill-height>
+      <v-flex xs12>
+        <v-card height='1400' color='primary'>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script charset="utf-8">
+export default {
+  name: "Records",
+  data: () => ({
+  })
+}
+</script>
---
+++ apps/wingu_web/assets/js/vue/routes/home/subroutes/company_routes/Stats.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid :class='$vuetify.breakpoint.smAndDown ? "mygradient pa-0 mt-5" :  "mygradient mt-5" '>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card height='1400' color='secondary'>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script charset="utf-8">
+export default {
+  name: "Stats",
+  data: () => ({
+  })
+}
+</script>
