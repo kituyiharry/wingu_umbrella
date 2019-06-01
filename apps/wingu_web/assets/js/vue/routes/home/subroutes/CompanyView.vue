36eb2f636327248f486b702070b300b1b753aed3
Harry Kituyi <kituyiharry@gmail.com>
2019-06-01 19:45:40 +0300

    Added list groups, prepare routing

--- apps/wingu_web/assets/js/vue/routes/home/subroutes/CompanyView.vue
+++ apps/wingu_web/assets/js/vue/routes/home/subroutes/CompanyView.vue
@@ -1,61 +1,81 @@
 <template>
   <v-app :dark='dark' style='font-family: "Montserrat", sans-serif;'> 
-    <!--:permanent='$vuetify.breakpoint.mdAndUp'-->
-      <!--:fixed='$vuetify.breakpoint.mdAndUp' -->
     <v-navigation-drawer
-      app fixed 
+      app 
+      :permanent='$vuetify.breakpoint.mdAndUp'
+      :fixed='$vuetify.breakpoint.mdAndUp' 
       :mini-variant="mini" 
       color='primary' v-model="drawer">
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg">
-            </v-list-tile-avatar>
+      <!--<v-layout fill-height>-->
+        <!--<v-navigation-drawer-->
+          <!--dark-->
+          <!--:mini-variant.sync="mini" -->
+          <!--value="true">-->
+          <v-toolbar flat class="transparent">
+            <v-list class="pa-0">
+              <v-list-tile avatar>
+                <v-list-tile-avatar>
+                  <img src="https://randomuser.me/api/portraits/men/85.jpg">
+                </v-list-tile-avatar>
 
-            <v-list-tile-content>
-              <v-list-tile-title>John Leider</v-list-tile-title>
-            </v-list-tile-content>
+                <v-list-tile-content>
+                  <v-list-tile-title>John Leider</v-list-tile-title>
+                </v-list-tile-content>
 
-            <v-list-tile-action>
-              <!--@click.stop="mini = !mini"-->
-              <v-btn
-                icon
-                >
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
+                <v-list-tile-action>
+                  <!--@click.stop="mini = !mini"-->
+                  <v-btn
+                    icon
+                    >
+                    <v-icon>chevron_left</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+          </v-toolbar>
+          <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+            <v-divider />
+            <v-list-group
+              prepend-icon="domain"
+              :value="true">
+              <template v-slot:activator>
+                <v-list-tile>
+                  <v-list-tile-title>Users</v-list-tile-title>
+                </v-list-tile>
+              </template>
+              <v-tooltip right transition='scale-transition'
+                v-for="(item,ind) in items"
+                :key="item.icon">
+                <v-list-tile slot='activator'>
+                  <v-list-tile-action>
+                    <div data-aos-offset='-1000000' data-aos='zoom-in' :data-aos-delay='ind*100'>
+                      <v-icon>{{ item.icon }}</v-icon>
+                    </div>
+                  </v-list-tile-action>
 
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-          @click=""
-          >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar flat app 
-      extension-height='98' extended
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <span>
+                  {{ item.icon }}
+                </span>
+              </v-tooltip>
+            </v-list-group>
+          </v-list>
+        </v-navigation-drawer>
+        <!--</v-layout>-->
+      <!--</v-navigation-drawer>-->
+    <v-toolbar height='72' flat app 
+      extension-height='102' extended
       dark color="primary">
       <!--<v-toolbar-side-icon @click='drawer=!drawer'/>-->
       <!--<v-fab-transition>-->
         <!--</v-fab-transition>-->
       <v-toolbar-title>
         <v-layout column>
-          <div style='text-overflow: scroll; font-family: Pacifico;font-weight: normal;font-size: 18px;'>
+          <div style='text-overflow: scroll; font-family: Pacifico;font-weight: normal;font-size: 20px;'>
             <!--<strong>{{ $store.state.store.home | first }}</strong>-->
             <strong>Cerrelo Studio</strong>
           </div>
@@ -65,32 +85,11 @@
         </v-layout>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <!--<v-menu-->
-        <!--attach='#chat' -->
-        <!--transition='slide-y-reverse-transition' -->
-        <!--nudge-left='170'-->
-        <!--nudge-bottom='4'-->
-        <!--open-on-hover close-on-content-click offset-y>-->
-        <!--<div id='chat' slot='activator'>-->
-          <!--<v-btn id='chat' outline data-aos='zoom-in' data-aos-delay='200' icon round flat>-->
-            <!--<v-badge color='red'>-->
-              <!--<span style='font-size: 12px;' slot='badge'>23</span>-->
-              <!--<v-icon small>-->
-                <!--chat-->
-                <!--</v-icon>-->
-              <!--</v-badge>-->
-            <!--</v-btn>-->
-          <!--</div>-->
-        <!--<div class='up-arrow'>-->
-          <!--</div>-->
-        <!--<v-card style='border-radius: 8px;' width='230' height='200'>-->
-          <!--</v-card>-->
-        <!--</v-menu>-->
-        <v-btn v-if='$vuetify.breakpoint.smAndDown' data-aos='zoom-in' data-aos-delay='100' icon @click='drawer=!drawer'>
-          <v-icon >
-            menu
-          </v-icon>
-        </v-btn>
+      <v-btn v-if='$vuetify.breakpoint.smAndDown' data-aos='zoom-in' data-aos-delay='100' icon @click='drawer=!drawer'>
+        <v-icon >
+          menu
+        </v-icon>
+      </v-btn>
       <v-menu 
         attach='#bell' 
         transition='slide-y-reverse-transition' 
@@ -139,7 +138,7 @@
       </v-menu>
       <div style='width:100%;' slot='extension'>
         <v-card color='transparent' flat>
-          <v-card-title class='pa-0' style='font-size: 32px;'>
+          <v-card-title class='pa-0' style='font-size: 36px;'>
             <strong>
               Overview
             </strong>
@@ -149,11 +148,13 @@
             </v-icon>
           </v-card-title>
           <v-card-actions class='pa-0'>
-            <v-tabs class='pa-0' show-arrows color='primary'
+            <v-tabs class='pa-0' color='primary'
               v-model="tab">
               <v-tabs-slider style='border-radius: 8px;' color="yellow"></v-tabs-slider>
-              <v-tab v-for="item in items" :key="item.icon">
-                {{ item.title }}
+              <v-tab :ripple='false' v-for="(item,ind) in items" :key="item.icon">
+                <v-btn round small :flat='tab!=ind' :class='tab==ind ? "secondary lighten-3 black--text" : ""'>
+                  {{ item.title }}
+                </v-btn>
               </v-tab>
             </v-tabs>
           </v-card-actions>
@@ -162,10 +163,10 @@
     </v-toolbar>
     <v-content>
       <v-tabs-items v-model="tab">
-        <v-tab-item v-for="item in items" :key="item.title">
+        <v-tab-item v-for="item in items" :key="item.icon">
           <v-container class='pa-0' fluid fill-height>
-            <v-layout row wrap>
-              <v-card flat>
+            <v-layout fill-height row wrap>
+              <v-card color='transparent' flat>
                 <v-card-text>
                   <p>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
@@ -215,6 +216,7 @@ export default {
     tab: 0,
     activeTab: 0,
     drawer: true,
+    links: ['Home', 'Contacts', 'Settings'],
     items: [
       { title: 'Home', icon: 'dashboard' },
       { title: 'Home', icon: 'account_circle' },
